3494eefc10f4116bec14cae2a2678519a19a458e
Wesley Moraes Benatti <wesleymbenatti@gmail.com>
2020-11-26 17:13:22 -0300

    About view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,12 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 
@@ -34,12 +42,44 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+		loadView("/gui/About.fxml");
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 
+	}
+
+	//synchronized garante que a aplicação gráfica não seja interrompida
+	private synchronized void loadView(String absoluteName) {
+		try {
+			//instanciando um FXML a partir de um caminho do projeto
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			//mainScene recebe o ScrollPane da classe Main
+			Scene mainScene = Main.getMainScene();
+			
+			//mainVBox pegando o VBox da tela principal
+			//getRoot (primeiro elemento da MainView = ScrollPane)
+			//getContent (VBox do MainView)
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			//childrens do VBox da MainView(MenuBar)
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			//limpar todos os filhos do mainVBox
+			mainVBox.getChildren().clear();
+			
+			//adiciona na tela principal a MenuBar
+			mainVBox.getChildren().add(mainMenu);
+			
+			//adiciona na tela principal, todos os filhos do newVBox
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
---
+++ src/gui/util/Constraints.java
@@ -0,0 +1,33 @@
+package gui.util;
+
+import javafx.scene.control.TextField;
+
+public class Constraints {
+	
+	public static void setTextFieldInteger(TextField txt) {
+		//aceitar apenas números inteiros no campo
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	public static void setTextFieldMaxLength(TextField txt, int max) {
+		//limite máximo de carácters no campo
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && newValue.length() > max) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+
+	public static void setTextFieldDouble(TextField txt) {
+		//aceitar apenas números doubles no campo
+		txt.textProperty().addListener((obs, oldValue, newValue) -> {
+			if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
+				txt.setText(oldValue);
+			}
+		});
+	}
+}
